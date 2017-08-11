cdf0e1ae429af1b7d827d971dc1e6e4d9ad8f679
Kit La Touche <kit@transneptune.net>
2017-08-11 16:47:02 -0600

    Initial commit

---
+++ murray/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+
+__version__ = '0.1.0'
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    return cur_dir
---
+++ setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""`murray` lives on `Github`_.
+.. _github: https://www.github.com/wlonk/murray
+"""
+from io import open
+from setuptools import setup
+from murray import __version__
+
+
+setup(
+    name='murray',
+    version=__version__,
+    url='https://github.com/wlonk/murray/',
+    license='MIT',
+    author='Kit La Touche',
+    author_email='kit@transneptune.net',
+    description='Murray theme for Sphinx',
+    long_description=open('README.rst', encoding='utf-8').read(),
+    zip_safe=False,
+    packages=['murray'],
+    package_data={'murray': [
+        'theme.conf',
+        '*.html',
+        'static/css/*.css',
+        'static/js/*.js',
+        'static/font/*.*'
+    ]},
+    include_package_data=True,
+    # See
+    # http://www.sphinx-doc.org/en/stable/theming.html
+    #     #distribute-your-theme-as-a-python-package
+    entry_points={
+        'sphinx.html_themes': [
+            'murray = murray',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
+)
