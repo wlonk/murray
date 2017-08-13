32beb95a811d4f2f22de745bfcbaec2960f85e25
Kit La Touche <kit@transneptune.net>
2017-08-13 01:13:30 -0600

    Possibly fix theme finding bugs

--- murray/__init__.py
+++ murray/__init__.py
@@ -1,10 +1,15 @@
 import os
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     return cur_dir
+
+
+def setup(app):
+    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    app.add_html_theme('murray', cur_dir)
