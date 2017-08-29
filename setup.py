6808552486fa42516b2a14b4a215e313938c30c4
Kit La Touche <kit@transneptune.net>
2017-08-29 12:28:45 -0600

    Add `setup.py publish` command

--- setup.py
+++ setup.py
@@ -2,9 +2,52 @@
 """`murray` lives on `Github`_.
 .. _github: https://www.github.com/wlonk/murray
 """
+import os
+import sys
+
 from io import open
-from setuptools import setup
+from setuptools import (
+    setup,
+    Command,
+)
+from shutil import rmtree
+
 from murray import __version__
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.sep.join(('.', 'dist')))
+        except FileNotFoundError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable),
+        )
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
 
 
 setup(
@@ -55,4 +98,8 @@ setup(
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'publish': PublishCommand,
+    },
 )
